8d0f3cbed64981ecfb8fb4b3c405722472d279a8
Julia Chaidez <jchaidez@jchaide-ltmc2wd.internal.salesforce.com>
2024-06-25 18:48:18 -0700

    API Endpoints working

--- student-store-api/models/orderModel.js
+++ student-store-api/models/orderModel.js
@@ -10,31 +10,29 @@ const getAllOrders = async (filter = {}, orderBy = {}) => {
 };
 
 // function to get order by ID
-const getOrderById = async (id) => {
-    return prisma.order.findUnique({where: {id:parseInt(id)}});
+const getOrderById = async (order_id) => {
+    return prisma.order.findUnique({where: {order_id:parseInt(order_id)}});
 };
 
 // function to create a new order
 const createOrder = async (orderData) => {
     return prisma.order.create({
         data: {
-            name: orderData.name,
-            category: orderData.category,
-            image_url: orderData.image_url,
-            description: orderData.description,
-            price: parseFloat(orderData.price),
+            customer_id: parseInt(orderData.customer_id),
+            total_price: parseFloat(orderData.total_price),
+            status: orderData.status,
         },
     });
 };
 
 // function to update a order
-const updateOrder = async (id, orderData) => {
-    return prisma.order.update({where: {id:parseInt(id)}, data: orderData,});
+const updateOrder = async (order_id, orderData) => {
+    return prisma.order.update({where: {order_id:parseInt(order_id)}, data: orderData,});
 };
 
 // function to delete a order
-const deleteOrder = async (id) => {
-    return prisma.order.delete({where: {id:parseInt(id)}});
+const deleteOrder = async (order_id) => {
+    return prisma.order.delete({where: {order_id:parseInt(order_id)}});
 };
   
 // export the functions
